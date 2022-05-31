a8e45f150038c24e7b961b2f2761dcaeb7b251d7
Damith Premasiri <damithpremasiri@gmail.com>
2022-05-31 11:30:25 +0100

    T5 model integration

--- examples/mwe/en/run.py
+++ examples/mwe/en/run.py
@@ -33,6 +33,29 @@ def set_sequence_and_clean(df):
     return df
 
 
+def set_sequence_and_clean_t5(df):
+    seq = np.zeros(len(df.index), dtype='i4')
+    df['sentence_id'] = seq
+
+    cur_sid = 1
+    lst = df.index[df['token_id'] == 1].to_list()
+    size = len(lst)
+    for i in range(0, size):
+        if i + 1 < size:
+            from_n = lst[i]
+            to_n = lst[i + 1]
+            for j in range(from_n, to_n):
+                df.loc[j, 'sentence_id'] = cur_sid
+        else:
+            from_n = lst[i]
+            for j in range(from_n, len(df.index)):
+                df.loc[j, 'sentence_id'] = cur_sid
+        cur_sid += 1
+    df['target_text'] = df['target_text'].str.upper()
+    df = df.drop(df[df['target_text'] == '0'].index)
+    return df
+
+
 def get_sentences(df_gold):
     sentences = []
     tokens = []
@@ -54,6 +77,26 @@ def get_sentences(df_gold):
             tokens.append(sub_list_token)
     return sentences, tokens
 
+def get_sentences_t5(df_gold):
+    sentences = []
+    tokens = []
+    cur_sid = 1
+    sub_list = []
+    sub_list_token = []
+    for i in range(0, len(df_gold.index)):
+        if cur_sid == df_gold.iloc[i]['sentence_id']:
+            sub_list.append(str(df_gold.iloc[i]['input_text']))
+            sub_list_token.append(df_gold.iloc[i]['target_text'])
+        else:
+            sentences.append(" ".join(sub_list))
+            tokens.append(sub_list_token)
+            cur_sid += 1
+            sub_list = [str(df_gold.iloc[i]['input_text'])]
+            sub_list_token = [df_gold.iloc[i]['target_text']]
+        if i == len(df_gold.index) - 1:
+            sentences.append(" ".join(sub_list))
+            tokens.append(sub_list_token)
+    return sentences, tokens
 
 def bert_based_evaluation(model):
     train_f = os.path.join(".", "examples", "mwe", "en", "data", "dimsum16.train")
---
+++ examples/mwe/en/run_t5.py
@@ -0,0 +1,63 @@
+import os
+import pandas as pd
+
+from examples.mwe.en.run import get_sentences, set_sequence_and_clean_t5, get_sentences_t5
+from mwe.t5.t5_model import T5Model
+from mwe.transformers.util.print_stat import print_information_multi_class
+
+MULTI_LABEL_CLASSIFICATION = "multilabel classification"
+
+model_args = {
+        "max_seq_length": 196,
+        "train_batch_size": 32,
+        "eval_batch_size": 64,
+        "num_train_epochs": 3,
+        "evaluate_during_training": False,
+        "evaluate_during_training_steps": 15000,
+        "evaluate_during_training_verbose": True,
+
+        "use_multiprocessing": False,
+        "fp16": False,
+
+        "save_steps": -1,
+        "save_eval_checkpoints": False,
+        "save_model_every_epoch": False,
+
+        "reprocess_input_data": True,
+        "overwrite_output_dir": True,
+
+        "wandb_project": "Multi-Label-Classification",
+    }
+
+def t5_based_evaluation():
+    print("T5 Model Based Predictions")
+
+    train_f = os.path.join(".", "examples", "mwe", "en", "data", "dimsum16.train")
+    gold_test_f = os.path.join(".", "examples", "mwe", "en", "data", "dimsum16.test")
+
+    df_train = pd.read_csv(train_f, usecols=[0, 1, 4], names=["token_id", "input_text", "target_text"], sep='\t')
+    df_train = set_sequence_and_clean_t5(df_train)
+    df_train["prefix"] = MULTI_LABEL_CLASSIFICATION
+    df_gold = pd.read_csv(gold_test_f, usecols=[0, 1, 4], names=["token_id", "input_text", "target_text"], sep='\t')
+    df_gold = set_sequence_and_clean_t5(df_gold)
+    df_gold["prefix"] = MULTI_LABEL_CLASSIFICATION
+
+    model = T5Model("t5", "t5-base", args=model_args)
+
+    model.train_model(df_train)
+
+    sentences, tokens = get_sentences_t5(df_gold)
+    tokens_to_predict = []
+    for sent in sentences:
+        for token in sent.split(" "):
+            tokens_to_predict.append(token)
+
+    predictions = model.predict(tokens_to_predict)
+
+    df_gold['predicted'] = predictions
+
+    print_information_multi_class(df_gold, "predicted", "target_text")
+
+
+if __name__ == '__main__':
+    t5_based_evaluation()
